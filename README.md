677d43de9ffca27378b4372a8c05d03830fb08c8
Paweł Sobolewski <pawel.sobolewski@ds-consultants.eu>
2025-01-29 13:41:19 +0100

    DXP-1480 Algolia JS as standalone CSS/JS/JS init

--- README.md
+++ README.md
@@ -93,3 +93,32 @@ This documentation outlines the components of the project, the purpose of each f
 ### Testing the Setup
 - Open a browser and navigate to the local proxy's URL (typically `http://localhost:8080`).
 - Verify the pages are loading correctly, and dynamic content from templates and assets is displayed as expected.
+
+
+---
+
+### Setting up autocomplete js
+As part of Streamx Accelerator we deliver an option to setup initial search via Algolia JS plugin called autocomplete-js.
+
+https://www.algolia.com/doc/ui-libraries/autocomplete/api-reference/autocomplete-js/autocomplete/
+
+It's a simple and isolated plugin with minimal setup. In orderd for it to work we need:
+- HTML element with an id of "autocomplete"(make sure to place it within desired area, ex: navigation bar):
+    <div id="autocomplete"></div>
+
+- Import required JS and CSS. For that we have 2 options either reuse minified versions of it from our repo:
+    <link rel="stylesheet" href="../web-resources/css/algolia.theme.min.css" />
+    <script src="../web-resources/js/autocomplete-js.js"></script>
+
+    or directly from CDN:
+
+    <script src="https://cdn.jsdelivr.net/npm/@algolia/autocomplete-js@1.18.0/dist/umd/index.production.js" integrity="sha256-Aav0vWau7GAZPPaOM/j8Jm5ySx1f4BCIlUFIPyTRkUM=" crossorigin="anonymous"></script>
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@algolia/autocomplete-theme-classic@1.17.9/dist/theme.min.css" integrity="sha256-7xmjOBJDAoCNWP1SMykTUwfikKl5pHkl2apKOyXLqYM=" crossorigin="anonymous"/>
+
+- Attach our custom main.publish.js to initialize it:
+    <script defer src="../web-resources/js/autocomplete-init.js"></script>
+
+Our custom autocomplete-init.js contains all of the required JS for the plugin to work. Alternatively you can follow the instructions from the setup section of autocomplete-js for higher level of control over it:
+
+https://www.algolia.com/doc/ui-libraries/autocomplete/api-reference/autocomplete-js/autocomplete/
--- web-resources/js/main.published.js
+++ web-resources/js/main.published.js
@@ -1,6 +1,5 @@
 !function() {
     const classToggle = (el, ...args) => args.map(e => el.classList.toggle(e));
-    const SEARCH_RESULTS_COUNT = 40;
     const K_SPACE = ' ';
     const C_SPACE = 32;
     const K_ENTER = 'Enter';
@@ -15,79 +14,6 @@
         classToggle(navMenuCloseIcon, 'block', 'hidden');
         classToggle(mobileMenu, 'block', 'hidden');
     }
-
-    const buildUrl = (query, limit) => {
-        return `/search/query?size=${limit}&query=${query}`;
-    };
-      
-    const mapToPagesResponse = async (response)=> {
-        const jsonResponse = await response.json();
-        const items = jsonResponse.hits.hits ? jsonResponse.hits.hits.map(mapToPage) : [];
-        return {
-          items: items,
-          executionTimeMs: jsonResponse.took
-        };
-    }
-
-    const getPages = async (query) => {
-        const response = await fetch(buildUrl(query, SEARCH_RESULTS_COUNT));
-        return mapToPagesResponse(response);
-    };
-
-    const getAutocompleteItemUrl = (item) => {
-        return (item && item.path) ? item.path : "";
-    };
-
-    const getItemTemplate = (html, item) => {
-        return html`<a class="aa-ItemLink" href=${getAutocompleteItemUrl(item)}>
-            <div class="aa-ItemContent">
-                <div class="aa-ItemIcon aa-ItemIcon--alignTop">
-                    <svg
-                        aria-labelledby="title"
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke="currentColor"
-                        stroke-width="2"
-                        class="block h-full w-auto"
-                        role="img"
-                        >
-                        <title id="title">Guide</title>
-                        <path
-                            d="M13 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V9z"
-                        ></path>
-                        <polyline points="13 2 13 9 20 9"></polyline>
-                    </svg>
-                </div>
-                <div class="aa-ItemContentBody">
-                    <div
-                        class="aa-ItemContentTitle"
-                        dangerouslySetInnerHTML="${ item.title }"
-                    ></div>
-                    <div
-                        class="aa-ItemContentDescription"
-                        dangerouslySetInnerHTML="${ item.bestFragment }"
-                    ></div>
-                </div>
-                <div class="aa-ItemActions">
-                    <button class="aa-ItemActionButton" type="button" title="Add to cart">
-                    <svg
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        stroke="currentColor"
-                        stroke-width="2"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                    >
-                        <polyline points="9 10 4 15 9 20"></polyline>
-                        <path d="M20 4v7a4 4 0 0 1-4 4H4"></path>
-                    </svg>
-                    </button>
-                </div>
-            </div>
-        </a>`;
-    };
 
     function init() {
         const navMenuMobileButton = document.getElementById('mobile-nav-button');
@@ -104,42 +30,6 @@
                 toggleMobileNavMenu();
             }
         });
-
-        const { autocomplete } = window['@algolia/autocomplete-js'];
-        autocomplete({
-            container: '#autocomplete',
-            placeholder: "Search...",
-            detachedMediaQuery: "",
-            openOnFocus: true,
-            getSources({ query }) {
-                return [
-                  {
-                    sourceId: "pages",
-                    async getItems({ query }) {
-                      if (query === "") {
-                        return [];
-                      }
-                      const pages = await getPages(query);
-                      return pages.items;
-                    },
-                    getItemUrl({ item }) {
-                      return getAutocompleteItemUrl(item);
-                    },
-                    templates: {
-                      item({ item, html }) {
-                        return getItemTemplate(html, item);
-                      },
-                      noResults:
-                        query === ""
-                          ? undefined
-                          : () => {
-                              return "No results";
-                            },
-                    },
-                  }
-                ]
-              },
-          });
     }
 
     init();
