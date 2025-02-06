21c5a2287455a1ac68b8d1c6f12bd5d9e69efd6b
Kamil Chociej <kamil.chociej@ds.pl>
2025-02-06 13:55:15 +0100

    [DXP-1512] GH variables and secrets

--- .github/scripts/set-repo-variables.sh
+++ .github/scripts/set-repo-variables.sh
@@ -5,6 +5,9 @@ gh variable set TF_VAR_CERT_MANAGER_LETS_ENCRYPT_ISSUER_ACME_EMAIL -b "$TF_VAR_c
 gh variable set TF_VAR_STREAMX_OPERATOR_IMAGE_PULL_SECRET_REGISTRY_EMAIL -b "$TF_VAR_streamx_operator_image_pull_secret_registry_email"
 gh variable set TF_VAR_RESOURCE_GROUP_NAME -b "$TF_VAR_resource_group_name"
 gh variable set TF_VAR_LOCATION -b "$TF_VAR_location"
-gh variable set INGESTION_HOST -b "$INGESTION_HOST"
-gh variable set WEB_HOST -b "$WEB_HOST"
-
+if [[ -n "$INGESTION_HOST" ]]; then
+    gh variable set INGESTION_HOST -b "$INGESTION_HOST"
+fi
+if [[ -n "$WEB_HOST" ]]; then
+    gh variable set WEB_HOST -b "$WEB_HOST"
+fi
