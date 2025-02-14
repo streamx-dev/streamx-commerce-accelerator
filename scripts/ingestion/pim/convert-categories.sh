a81bbb02d1877b44f7ae1ff9a23c67c942e7ee79
Michal Cukierman <michal.cukierman@ds.pl>
2025-02-14 14:45:31 +0100

    DXP-1650 Ingestion Publication using CLI

---
+++ scripts/ingestion/pim/convert-categories.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+echo "Converting categories into StreamX format..."
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+input_file="$SCRIPT_DIR/../../../data/catalog/categories.json"
+output_file="$SCRIPT_DIR/../../../data/catalog/categories.stream"
+rm "$output_file"
+
+json_data=$(cat "$input_file")
+
+echo "$json_data" | jq -c '.[]' | while IFS= read -r category; do
+  id=$(echo "$category" | jq -r '.id')
+
+  processed_category=$(jq -n --arg id "$id" --argjson content "$category" --arg prefix "cat:" \
+  '{
+      "key": ($prefix + $id),
+      "action": "publish",
+      "eventTime": null,
+      "properties": {},
+      "payload": {
+        "dev.streamx.blueprints.data.Data": {
+          "content": {
+            "bytes": ($content | @json)
+          }
+        }
+      }
+  }')
+
+  echo "$processed_category" >> "$output_file"
+done
+
+echo "Categories conversion finished"
