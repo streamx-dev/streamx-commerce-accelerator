b2e8275c3b2b9137c5cd2becab48d713be7aa8b5
Marek Czajkowski <76772327+marekczajkowski@users.noreply.github.com>
2025-02-14 14:23:00 +0100

    [DXP-1654] Support public static ip on azure (#31)

--- .github/scripts/set-repo-variables.sh
+++ .github/scripts/set-repo-variables.sh
@@ -6,13 +6,13 @@ gh variable set TF_VAR_STREAMX_OPERATOR_IMAGE_PULL_SECRET_REGISTRY_EMAIL -b "$TF
 gh variable set TF_VAR_RESOURCE_GROUP_NAME -b "$TF_VAR_resource_group_name"
 gh variable set TF_VAR_LOCATION -b "$TF_VAR_location"
 
-if [[ -n "$TF_VAR_USER_IDENTITY_ID" ]]; then
+if [[ -n "$TF_VAR_user_identity_id" ]]; then
 gh variable set TF_VAR_USER_IDENTITY_ID -b "$TF_VAR_user_identity_id"
 fi
-if [[ -n "$TF_VAR_PUBLIC_IP_ADDRESS" ]]; then
+if [[ -n "$TF_VAR_public_ip_address" ]]; then
 gh variable set TF_VAR_PUBLIC_IP_ADDRESS -b "$TF_VAR_public_ip_address"
 fi
-if [[ -n "$TF_VAR_PUBLIC_IP_ID" ]]; then
+if [[ -n "$TF_VAR_public_ip_id" ]]; then
 gh variable set TF_VAR_PUBLIC_IP_ID -b "$TF_VAR_public_ip_id"
 fi
 if [[ -n "$INGESTION_HOST" ]]; then
