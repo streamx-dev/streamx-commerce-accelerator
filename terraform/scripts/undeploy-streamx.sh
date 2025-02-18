3eee5cc91f930dda120c5b7a3033ca4f8dcf42cc
Kamil Chociej <kamil.chociej@ds.pl>
2025-02-18 15:35:41 +0100

    [DXP-1650] terraform init in undeploy-streamx.sh

--- terraform/scripts/undeploy-streamx.sh
+++ terraform/scripts/undeploy-streamx.sh
@@ -6,4 +6,5 @@ if [ -e "$DESTROY_ENV_SCRIPT_DIR/../azure/.env" ]; then
   source "$DESTROY_ENV_SCRIPT_DIR/read-infra-env.sh" "$DESTROY_ENV_SCRIPT_DIR/../azure/.env"
 fi
 
+terraform -chdir="$DESTROY_ENV_SCRIPT_DIR"/../azure/platform init
 terraform -chdir="$DESTROY_ENV_SCRIPT_DIR"/../azure/platform destroy -auto-approve
