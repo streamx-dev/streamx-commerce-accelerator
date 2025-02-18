c44c546e79c6a46e3bb014ff8e9b89f62c2e6058
Kamil Chociej <kamil.chociej@ds.pl>
2025-02-18 13:35:27 +0100

    [DXP-1650] adjusted paths for GH actions

--- terraform/scripts/undeploy-streamx.sh
+++ terraform/scripts/undeploy-streamx.sh
@@ -2,8 +2,8 @@
 set -e
 
 DESTROY_ENV_SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-if [ -e "$DESTROY_ENV_SCRIPT_DIR/../../terraform/azure/.env" ]; then
-  source "$DESTROY_ENV_SCRIPT_DIR/read-infra-env.sh" "$DESTROY_ENV_SCRIPT_DIR/../../terraform/azure/.env"
+if [ -e "$DESTROY_ENV_SCRIPT_DIR/../azure/.env" ]; then
+  source "$DESTROY_ENV_SCRIPT_DIR/read-infra-env.sh" "$DESTROY_ENV_SCRIPT_DIR/../azure/.env"
 fi
 
-terraform -chdir="$DESTROY_ENV_SCRIPT_DIR"/../../terraform/azure/platform destroy -auto-approve
+terraform -chdir="$DESTROY_ENV_SCRIPT_DIR"/../azure/platform destroy -auto-approve
