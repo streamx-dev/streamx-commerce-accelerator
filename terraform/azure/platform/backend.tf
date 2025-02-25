996ab17e1a045003546cc289736040d447571f9b
Marek Czajkowski <76772327+marekczajkowski@users.noreply.github.com>
2025-02-25 13:38:33 +0100

    [DXP-1569] API Gateway (#45)
    * Products keywords corrected
    
    * Apisix gateway
    
    * Apisix gateway
    
    * [DXP-1481] Product data change
    
    * [DXP-1481] Product data change
    
    * apisix
    
    * apisix
    
    * apisix
    
    * apisix
    
    * apisix
    
    * apisix
    
    * apisix
    
    * apisix
    
    * apisix
    
    * apisix
    
    * apisix
    
    * apisix
    
    * apisix
    
    * apisix
    
    * apisix
    
    * apisix
    
    * apisix
    
    * apisix
    
    * apisix
    
    * apisix
    
    * apisix
    
    * apisix
    
    * apisix
    
    * apisix
    
    * apisix
    
    * apisix
    
    * apisix

--- terraform/azure/platform/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "demo-puresight-starter"
-    storage_account_name = "tfstateyfyg6"
-    container_name       = "streamx-commerce-accelerator-tfstate"
-    key                  = "platform.tfstate"
-  }
-}
