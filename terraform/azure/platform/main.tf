751240db42a7a82bec6b665a30b0e1c12028bb01
Kamil Chociej <38164671+kamilchociej@users.noreply.github.com>
2025-02-25 11:50:51 +0100

    Update main.tf
    Signed-off-by: Kamil Chociej <38164671+kamilchociej@users.noreply.github.com>

--- terraform/azure/platform/main.tf
+++ terraform/azure/platform/main.tf
@@ -34,7 +34,7 @@ module "streamx" {
   cert_manager_lets_encrypt_issuer_acme_email              = var.cert_manager_lets_encrypt_issuer_acme_email
   cert_manager_lets_encrypt_issuer_prod_letsencrypt_server = true
   ingress_controller_nginx_settings                        = var.public_ip_address == null || var.public_ip_address == "" ? local.ingress_controller_nginx_settings_without_static_ip : local.ingress_controller_nginx_settings_with_static_ip
-  pulsar_kaap_values                                       = [
+  pulsar_kaap_values = [
     file("${path.module}/config/pulsar-kaap/values.yaml")
   ]
   streamx_operator_image_pull_secret_registry_email    = var.streamx_operator_image_pull_secret_registry_email
