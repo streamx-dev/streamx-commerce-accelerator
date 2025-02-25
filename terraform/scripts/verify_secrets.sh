1d26d321100b091b42401c4f5a8dfba7574566f1
Marek Czajkowski <marek.czajkowski@ds.pl>
2025-02-25 10:38:32 +0100

    apisix

--- terraform/scripts/verify_secrets.sh
+++ terraform/scripts/verify_secrets.sh
@@ -13,7 +13,7 @@ for cert_file in "$VERIFY_SECRETS_SCRIPT_DIR"/../../gateway/tls/*.crt.yaml; do
     [ -f "$cert_file" ] || continue
     cert_name=$(basename "$cert_file" .yaml)
     if ! diff <(kubectl get secret "$cert_name" -o yaml | yq '.data."tls.crt"') <(yq eval '.data."tls.crt"' "$cert_file") >/dev/null; then
-        echo "Certificate $cert_name is different!. Make sure your k8s secrets match the ones from GH repository"
+        echo "The certificate $cert_name is different! It's likely that the certificates in Kubernetes are newer than those in your repository. Please update your local certificates and GitHub Action secrets to align with the newly generated ones in Kubernetes."
         exit 1
     fi
 done
@@ -22,7 +22,7 @@ for auth_key_file in "$VERIFY_SECRETS_SCRIPT_DIR"/../../mesh/auth/*.yaml; do
     [ -f "$auth_key_file" ] || continue
     auth_key_name=$(basename "$auth_key_file" .yaml)
     if ! diff <(kubectl get secret "$auth_key_name" -o yaml | yq '.data."private.key"') <(yq eval '.data."private.key"' "$auth_key_file") >/dev/null; then
-        echo "Private key $auth_key_name is different!. Make sure your k8s secrets match the ones from GH repository"
+        echo "Private key $auth_key_name is different!. Make sure your K8s secrets match the ones from GH repository"
         exit 1
     fi
 done
