97943075143733eb110976d10d45dc274acc6695
PRFTRyan <ryan.mccullough@perficient.com>
2025-02-28 11:52:31 -0800

    RM:Adding category to page data-attributes for tracking

--- scripts/publish-products.sh
+++ scripts/publish-products.sh
@@ -1,7 +1,6 @@
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 pushd "${SCRIPT_DIR}/../" || exit
 
-streamx batch publish data
 streamx stream data data/catalog/products.stream
 streamx stream data data/catalog/categories.stream
 
