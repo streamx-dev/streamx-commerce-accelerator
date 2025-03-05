24f96cf93a39cf7dcd7e92cad819e68b51e4256e
Marek Czajkowski <marek.czajkowski@ds.pl>
2025-03-05 11:03:34 +0100

    Env sizes

--- terraform/azure/monitoring-storage/variables.tf
+++ terraform/azure/monitoring-storage/variables.tf
@@ -17,7 +17,7 @@ variable "azurerm_storage_container_name" {
 }
 
 variable "storage_account_name_prefix" {
-  default     = "streamx-commerce-accelerator-monitoring"
+  default     = "streamx-monitoring"
   description = "Azure Storage Account name prefix."
   type        = string
 }
