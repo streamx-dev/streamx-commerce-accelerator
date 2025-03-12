834bedac904fcc70913ed7696fb2bb4e06d67aee
Rafal Glowacz <rafal.glowacz@ds.pl>
2025-03-12 11:35:02 +0100

    DXP-1522 Update demo-puresight-starter
    * DXP-1522 Update demo-puresight-starter
    * structure reorganisation
    * added articles on a homepage
    
    * DXP-1522 Update demo-puresight-starter
    * structure reorganisation
    * added articles on a homepage
    
    * DXP-1522 Update demo-puresight-starter
    * homepage tiles

--- scripts/publish-all.sh
+++ scripts/publish-all.sh
@@ -2,6 +2,7 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 pushd "${SCRIPT_DIR}/../" || exit
 
 streamx batch publish data
+streamx batch publish data/pages/puresight
 streamx stream data data/catalog/products.stream
 streamx stream data data/catalog/categories.stream
 
