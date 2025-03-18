dc8473dc430571e9f4b4b2511eb243f136dedef9
Marek Czajkowski <76772327+marekczajkowski@users.noreply.github.com>
2025-03-18 08:20:39 -0700

    Feature/dxp 1740 add puresight demo (#25)
    * DXP-1522 Update demo-puresight-starter
    * structure reorganisation
    * added articles on a homepage
    
    * DXP-1522 Update demo-puresight-starter
    * structure reorganisation
    * added articles on a homepage
    
    * DXP-1522 Update demo-puresight-starter
    * homepage tiles
    
    * DXP-1522 Update demo-puresight-starter
    * convert-all script for webp images
    
    * Revert "DXP-1522 Update demo-puresight-starter"
    
    This reverts commit 72561988d6ea3cbd5a32dc0d5bc4878510e98865.
    
    * DXP-1522 Update demo-puresight-starter
    * webp images for product listing tiles
    
    * DXP-1522 Update demo-puresight-starter
    * added webp image for homepage
    
    * DXP-1522 Update demo-puresight-starter
    * added related product for PDP bottom
    
    * DXP-1522 Update demo-puresight-starter
    * unclosed tag on pdp
    
    * DXP-1798 fix picture source path according to breakpoints, correct dpd main image style
    
    * DXP-1740: Add PureSight demo
    
    * [DXP-1740] fix old docker compose command
    
    * Key rewrite + search
    
    * Key rewrite + search
    
    * No prices products
    
    * No prices products
    
    * No prices products
    
    * No prices products
    
    * No prices products
    
    ---------
    
    Co-authored-by: Rafał Głowacz <rafal.glowacz@ds.pl>
    Co-authored-by: Martyna Szeszko <martyna.szeszko@ds.pl>
    Co-authored-by: Herman Ciechanowiec <herman.ciechanowiec@ds.pl>
    Co-authored-by: Pawel Boguski <pawel.boguski@ds.pl>

--- terraform/scripts/cloud-publish-all.sh
+++ terraform/scripts/cloud-publish-all.sh
@@ -4,6 +4,7 @@ pushd "${SCRIPT_DIR}/../../" || exit
 export QUARKUS_PROFILE=cloud,cms && streamx batch publish data
 export QUARKUS_PROFILE=cloud,pim && streamx stream data data/catalog/products-no-prices.stream
 export QUARKUS_PROFILE=cloud,pim && streamx stream data data/catalog/products.stream
+export QUARKUS_PROFILE=cloud,pim && streamx stream data data/catalog/products-no-prices.stream
 export QUARKUS_PROFILE=cloud,pim && streamx stream data data/catalog/categories.stream
 
 popd || exit
