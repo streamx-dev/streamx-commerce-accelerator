d9f8402a67d1f61036eabb1bd36ef3582f2d35b2
Marek Czajkowski <marek.czajkowski@ds.pl>
2025-06-23 15:16:45 +0200

    Updated helm provider version

--- terraform/azure/platform/main.tf
+++ terraform/azure/platform/main.tf
@@ -55,7 +55,7 @@ module "grafana_secret" {
 
 module "streamx" {
   source  = "streamx-dev/charts/helm"
-  version = "0.0.6"
+  version = "0.0.13"
 
   ingress_controller_nginx_enabled                         = false
   cert_manager_lets_encrypt_issuer_acme_email              = var.cert_manager_lets_encrypt_issuer_acme_email
