31fd6c0f9176c27572882c183ed4cc94e9f6abbd
Rafal Glowacz <rafal.glowacz@ds.pl>
2025-10-09 13:37:41 +0200

    SXPR-145 Add external-resources-processing-service to ingest images f… (#102)
    * SXPR-145 Add external-resources-processing-service to ingest images from PIM data source
    
    * SXPR-145 Add external-resources-processing-service to ingest images from PIM data source
    * align mesh configuration for reference images according to the model

--- scripts/catalog/convert-products.sh
+++ scripts/catalog/convert-products.sh
@@ -10,14 +10,19 @@ json_data=$(cat "$input_file" | sed 's/\\"/\&quot;/g')
 
 echo "$json_data" | jq -c '.[]' | while IFS= read -r product; do
   id=$(echo "$product" | jq -r '.id')
+  sx_type="data/product";
+  # Product ID:B07YBHC881 with fetch image references feature enable
+  if [[ "$id" == "B07YBHC881" ]]; then
+    sx_type="data/product_with_images";
+  fi
 
-  processed_product=$(jq -n --arg kek "$id" --argjson content "$product" --arg prefix "pim:" \
+  processed_product=$(jq -n --arg kek "$id" --argjson content "$product" --arg prefix "pim:" --arg sxtype "$sx_type" \
   '{
       "key": ($prefix + $kek),
       "action": "publish",
       "eventTime": null,
       "properties": {
-        "sx:type": "data/product"
+        "sx:type": ($sxtype)
       },
       "payload": {
         "dev.streamx.blueprints.data.Data": {
